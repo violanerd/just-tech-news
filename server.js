8638e7139447a5d6ff01ac459a1bdbfdbd5720ab
violanerd <madalyne.cross@gmail.com>
2022-08-22 14:13:51 -0500

    added login functionality

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
